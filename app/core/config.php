7fa26a182971e3756006c391a7eeffab99703f77
GuruinderJain <gujain@algomau.ca>
2024-07-21 22:43:42 +0000

    Initial commit

---
+++ app/core/config.php
@@ -0,0 +1,10 @@
+<?php
+// Database config
+define('DB_HOST', 'upu.h.filess.io');
+define('DB_PORT', '3305');
+define('DB_NAME', 'COSC4808Project_churchall');
+define('DB_USER', 'COSC4808Project_churchall');
+define('DB_PASS', $_ENV['DATABASE_PASS']);
+
+
+?>
